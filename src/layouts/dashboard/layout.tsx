a57905dd4fc4bd7664f121305a040bff35d0473b
Zabashtevich <zabashtevich@gmail.com>
2024-11-15 21:05:15 +0300

    исправил мобильное меню

--- src/layouts/dashboard/layout.tsx
+++ src/layouts/dashboard/layout.tsx
@@ -6,19 +6,21 @@ import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
 import { useTheme } from '@mui/material/styles';
 
-import { _notifications } from 'src/_mock';
+import { _langs, _notifications } from 'src/_mock';
 
 import { Iconify } from 'src/components/iconify';
 
 import { Main } from './main';
 import { layoutClasses } from '../classes';
-import { NavDesktop } from './nav';
+import { NavMobile, NavDesktop } from './nav';
 import { navData } from '../config-nav-dashboard';
-// import { _workspaces } from '../config-nav-workspace';
+import { Searchbar } from '../components/searchbar';
+import { _workspaces } from '../config-nav-workspace';
 import { MenuButton } from '../components/menu-button';
 import { LayoutSection } from '../core/layout-section';
 import { HeaderSection } from '../core/header-section';
 import { AccountPopover } from '../components/account-popover';
+import { LanguagePopover } from '../components/language-popover';
 import { NotificationsPopover } from '../components/notifications-popover';
 
 // ----------------------------------------------------------------------
@@ -34,7 +36,7 @@ export type DashboardLayoutProps = {
 export function DashboardLayout({ sx, children, header }: DashboardLayoutProps) {
   const theme = useTheme();
 
-  const [, setNavOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
 
   const layoutQuery: Breakpoint = 'lg';
 
@@ -60,16 +62,21 @@ export function DashboardLayout({ sx, children, header }: DashboardLayoutProps) 
               </Alert>
             ),
             leftArea: (
-              <MenuButton
-                onClick={() => setNavOpen(true)}
-                sx={{
-                  ml: -1,
-                  [theme.breakpoints.up(layoutQuery)]: { display: 'none' },
-                }}
-              />
+              <>
+                <MenuButton
+                  onClick={() => setNavOpen(true)}
+                  sx={{
+                    ml: -1,
+                    [theme.breakpoints.up(layoutQuery)]: { display: 'none' },
+                  }}
+                />
+                <NavMobile data={navData} open={navOpen} onClose={() => setNavOpen(false)} />
+              </>
             ),
             rightArea: (
               <Box gap={1} display="flex" alignItems="center">
+                <Searchbar />
+                <LanguagePopover data={_langs} />
                 <NotificationsPopover data={_notifications} />
                 <AccountPopover
                   data={[
