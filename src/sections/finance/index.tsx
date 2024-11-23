c14606509fba2be83b38757af9ff5dba47c38e4b
Zabashtevich <zabashtevich@gmail.com>
2024-11-24 02:46:41 +0300

    страница расходов

--- src/sections/finance/index.tsx
+++ src/sections/finance/index.tsx
@@ -1,5 +1,15 @@
-import { Card, Grid2, Typography } from '@mui/material';
-import { LineChart } from '@mui/x-charts';
+import {
+  Box,
+  Card,
+  Grid2,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import { LineChart, PieChart } from '@mui/x-charts';
 import { DashboardContent } from 'src/layouts/dashboard';
 
 const pData = [0, 190, 20, 300, 100, 200, 500, 150];
@@ -31,7 +41,251 @@ export const FinanceView = () => (
         </Card>
       </Grid2>
 
-      <Grid2 size={{ lg: 6, md: 6, xs: 12, sm: 12 }}>123</Grid2>
+      <Grid2 size={{ lg: 6, md: 6, xs: 12, sm: 12 }}>
+        <Card sx={{ padding: '24px', height: '100%' }}>
+          <Typography variant="h3" mb={4.5}>
+            Ваши траты
+          </Typography>
+
+          <PieChart
+            series={[
+              {
+                data: [
+                  { id: 0, value: 10, label: 'Маркировка' },
+                  { id: 1, value: 15, label: 'Упаковка' },
+                  { id: 2, value: 20, label: 'Иные' },
+                ],
+              },
+            ]}
+            width={400}
+            height={200}
+          />
+        </Card>
+      </Grid2>
+
+      <Card sx={{ padding: '24px', margin: '0 auto', flex: 1 }}>
+        <Typography variant="h3" textAlign="left" mb={2.5}>
+          Баланс
+        </Typography>
+
+        <Typography variant="h2" textAlign="left" mb={4.5} fontWeight={600} color="success">
+          236 110 Р
+        </Typography>
+
+        <Typography variant="h3" textAlign="center" mb={4.5}>
+          Детализация расходов
+        </Typography>
+
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Название</TableCell>
+              <TableCell>Сумма</TableCell>
+              <TableCell align="center">Дата</TableCell>
+              <TableCell align="right">Остаток на момент транзакции</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography fontWeight={700}>Пополнение баланса</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="success">4000</Typography>
+              </TableCell>
+              <TableCell align="center">22.11.2024</TableCell>
+              <TableCell align="right">
+                <Typography color="error">-3000</Typography>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Card>
     </Grid2>
   </DashboardContent>
 );
